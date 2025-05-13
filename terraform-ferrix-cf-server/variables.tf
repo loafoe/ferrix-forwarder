189d005225511cebbeccf5868e81c972638f6bd3
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2025-05-13 11:02:08 +0200

    Update ferrix forwarder client and server image tags to v0.0.2
    Signed-off-by: Andy Lo-A-Foe <andy.loafoe@gmail.com>

--- terraform-ferrix-cf-server/variables.tf
+++ terraform-ferrix-cf-server/variables.tf
@@ -22,10 +22,10 @@ variable "region" {
 
 variable "ferrix_forwarder_client_image" {
   type    = string
-  default = "ghcr.io/loafoe/ferrix-forwarder-client:v0.0.1"
+  default = "ghcr.io/loafoe/ferrix-forwarder-client:v0.0.2"
 }
 
 variable "ferrix_forwarder_server_image" {
   type    = string
-  default = "ghcr.io/loafoe/ferrix-forwarder-server:v0.0.1"
+  default = "ghcr.io/loafoe/ferrix-forwarder-server:v0.0.2"
 }
